0f0926a91c7ac5c4a54a22a126932b52786d0a8e
Polina Pavlenko <polina.pavvlenko@gmail.com>
2025-11-10 14:54:13 +0200

    feat: build login page

---
+++ app/(logged-out)/login/page.tsx
@@ -0,0 +1,115 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { PersonStandingIcon } from "lucide-react";
+import Link from "next/link";
+import { useForm } from "react-hook-form";
+import z, { email } from "zod";
+
+const formSchema = z.object({
+  email: z.email(),
+  password: z.string(),
+});
+function LoginPage() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  const handleSubmit = () => {
+    console.log("login validation passed");
+  };
+  return (
+    <>
+      <PersonStandingIcon size={50} className="text-pink-500" />
+      <Card className="w-full max-w-sm">
+        <CardHeader>
+          <CardTitle>Login</CardTitle>
+          <CardDescription>Login to your SupportMe account</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <Form {...form}>
+            <form
+              onSubmit={form.handleSubmit(handleSubmit)}
+              className="flex flex-col gap-4"
+            >
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => {
+                  return (
+                    <FormItem>
+                      <FormLabel>Email</FormLabel>
+                      <FormControl>
+                        <Input
+                          placeholder="jane@doe.com"
+                          type="email"
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormDescription>
+                        This is the email address you signed up to SupportMe
+                        with
+                      </FormDescription>
+                      <FormMessage />
+                    </FormItem>
+                  );
+                }}
+              />
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => {
+                  return (
+                    <FormItem>
+                      <FormLabel>Password</FormLabel>
+                      <FormControl>
+                        <Input
+                          placeholder="password"
+                          type="password"
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  );
+                }}
+              />
+              <Button type="submit">Login</Button>
+            </form>
+          </Form>
+        </CardContent>
+        <CardFooter className="justify-between">
+          <small>Don&apos;t have an account?</small>
+          <Button variant="outline" size="sm" asChild>
+            <Link href="signup">Sign Up</Link>
+          </Button>
+        </CardFooter>
+      </Card>
+    </>
+  );
+}
+
+export default LoginPage;
