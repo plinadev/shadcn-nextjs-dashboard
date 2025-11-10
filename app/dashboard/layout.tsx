9391947d7f1cc98f2b5f66bc8d0bb33e132ee7c5
Polina Pavlenko <polina.pavvlenko@gmail.com>
2025-11-10 20:33:50 +0200

    feat: add mobile menu

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,11 +1,28 @@
+"use client";
+import { Drawer, DrawerContent, DrawerTrigger } from "@/components/ui/drawer";
 import MainMenu from "./components/main-menu";
+import MenuTitle from "./components/menu-title";
+import { MenuIcon } from "lucide-react";
+import { useMediaQuery } from "@/hooks/use-media-query";
 
 function DashboardLayout({ children }: { children: React.ReactNode }) {
+  const isDesktop = useMediaQuery("(min-width: 768px)");
   return (
-    <div className="grid grid-cols-[250px_1fr] h-screen">
-      <div className="bg-muted overflow-auto py-2 px-4">
-        <MainMenu />
-      </div>
+    <div className="md:grid md:grid-cols-[250px_1fr] h-screen">
+      <MainMenu className="hidden md:flex" />
+      {!isDesktop && (
+        <div className="p-4 flex justify-between md:hidden sticky top-0 left-0 bg-background border-b border-border">
+          <MenuTitle />
+          <Drawer direction="right">
+            <DrawerTrigger>
+              <MenuIcon />
+            </DrawerTrigger>
+            <DrawerContent>
+              <MainMenu />
+            </DrawerContent>
+          </Drawer>
+        </div>
+      )}
       <div className="overflow-auto py-2 px-4">
         <h1 className="pb-4">Welcome back, Jane!</h1>
         {children}
