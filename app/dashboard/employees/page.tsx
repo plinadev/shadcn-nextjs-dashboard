3720914234d6bdf42546e6bb70a906739240ea72
Polina Pavlenko <polina.pavvlenko@gmail.com>
2025-11-11 11:02:17 +0200

    feat: add employees table

--- app/dashboard/employees/page.tsx
+++ app/dashboard/employees/page.tsx
@@ -1,5 +1,89 @@
-function EmployeesPage() {
-  return <div>emploees page</div>;
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { DataTable } from "@/components/ui/data-table";
+import { columns, type Employee } from "./columns";
+
+const employees: Employee[] = [
+  {
+    id: 1,
+    firstName: "Colin",
+    lastName: "Murray",
+    teamName: "alpha",
+    isTeamLeader: true,
+    avatar: "/images/cm.jpg",
+  },
+  {
+    id: 2,
+    firstName: "Tom",
+    lastName: "Phillips",
+    teamName: "alpha",
+    isTeamLeader: false,
+  },
+  {
+    id: 3,
+    firstName: "Liam",
+    lastName: "Fuentes",
+    teamName: "alpha",
+    isTeamLeader: false,
+  },
+  {
+    id: 4,
+    firstName: "Tina",
+    lastName: "Fey",
+    teamName: "canary",
+    isTeamLeader: true,
+    avatar: "/images/tf.jpg",
+  },
+  {
+    id: 5,
+    firstName: "Katie",
+    lastName: "Johnson",
+    teamName: "canary",
+    isTeamLeader: false,
+  },
+  {
+    id: 6,
+    firstName: "Tina",
+    lastName: "Jones",
+    teamName: "canary",
+    isTeamLeader: false,
+  },
+  {
+    id: 7,
+    firstName: "Amy",
+    lastName: "Adams",
+    teamName: "delta",
+    isTeamLeader: true,
+  },
+  {
+    id: 8,
+    firstName: "Ryan",
+    lastName: "Lopez",
+    teamName: "delta",
+    isTeamLeader: false,
+    avatar: "/images/rl.jpg",
+  },
+  {
+    id: 9,
+    firstName: "Jenny",
+    lastName: "Jones",
+    teamName: "delta",
+    isTeamLeader: false,
+  },
+];
+
+async function EmployeesPage() {
+  // await new Promise((resolve) => setTimeout(resolve, 3000));
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>Employees</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <DataTable columns={columns} data={employees} />
+        
+      </CardContent>
+    </Card>
+  );
 }
 
 export default EmployeesPage;
